1c97396b7817bce5cbee4ff9ff29c81134fc5f3d
corinnereloj <2015081450@ust-ics.mygbiz.com>
2018-09-06 13:44:24 +0800

    Initial commit

---
+++ app/src/main/java/com/reloj/corinne/MainActivity.java
@@ -0,0 +1,15 @@
+package com.reloj.corinne;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear);
+//        setContentView(R.layout.activity_constraint);
+//        setContentView(R.layout.activity_relative);
+    }
+}
